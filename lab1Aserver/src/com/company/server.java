ae3a0a88d228d9fc78613223b45aee6cc8850ba3
luben <luben93@gmail.com>
2015-09-23 12:58:47 +0200

    removed unused code

--- lab1Aserver/src/com/company/server.java
+++ lab1Aserver/src/com/company/server.java
@@ -19,8 +19,6 @@ public class Server {
     public void serve() {
         // write your code here
 
-
-
         //Socket clientSocket = null;
         System.out.println("Waiting for connection.....");
         try {
@@ -30,9 +28,7 @@ public class Server {
             serverSocket.receive(packet);
             String message = new String(packet.getData(), 0, packet.getLength());
 
-
-            // Print information
-            InetAddress clientAddr = packet.getAddress();
+            InetAddress clientAddr = packet.getAddress();// Print information
             System.out.println("Packet received from " + clientAddr.getHostName());
             System.out.println("Packet contained: " + message);
             byte[] FbufOut = "oh hai\n i will be your server today\n if i am am busy, i will tell you\n to play send\n HELLO and then\n START and then\n GUESS X where X is your guess number.".getBytes();
@@ -40,7 +36,7 @@ public class Server {
             serverSocket.send(Fout);
             buffer = new byte[bufferSize];
             packet = new DatagramPacket(buffer, buffer.length);
-            LinkedList<InetAddress> clients=new LinkedList<>();
+            LinkedList<InetAddress> clients = new LinkedList<>();
             Protocol p = new Protocol();
 
             while (true) {//TODO inte while true, det är så internet dör
@@ -49,20 +45,14 @@ public class Server {
                 serverSocket.receive(packet);
                 message = new String(packet.getData(), 0, packet.getLength());
 
-                System.out.println("Packet received from " + packet.getAddress().getHostName() +":"+packet.getPort());
+                System.out.println("Packet received from " + packet.getAddress().getHostName() + ":" + packet.getPort());
                 System.out.println("Packet contained:" + message + ":");//TODO remove last char in message, its a fucking \n
-                if (packet.getAddress().equals( clientAddr)) {
-                    // Print information
-                    send(p.game(message),clientAddr,packet.getPort());
-                    //start protocol
+                if (packet.getAddress().equals(clientAddr)) { // Print information
+                    send(p.game(message), clientAddr, packet.getPort()); //start protocol
                     System.out.println("is equalt");
-
                 } else {
                     System.out.println("is not eqaul");
-                   // byte[] bufOut = "BUSY".getBytes();
-                   // DatagramPacket out = new DatagramPacket(bufOut, bufOut.length, packet.getAddress(), packet.getPort());
-                   // serverSocket.send(out);
-                    send("BUSY",packet.getAddress(),packet.getPort());
+                    send("BUSY", packet.getAddress(), packet.getPort());
                     //  clients.push(packet.getAddress());
                     System.out.println("sent busy");
                 }
@@ -75,12 +65,10 @@ public class Server {
             System.out.println("UDPServer done.");
         }
         System.exit(0);
-
-
     }
 
-    public Server(int port){
-        this.port=port;
+    public Server(int port) {
+        this.port = port;
         try {
             serverSocket = new DatagramSocket(port);
         } catch (IOException e) {
@@ -90,9 +78,9 @@ public class Server {
 
     }
 
-    private void send(String msg,InetAddress recv,int recvPort)throws IOException{
+    private void send(String msg, InetAddress recv, int recvPort) throws IOException {
         byte[] bufOut = msg.getBytes();
-        DatagramPacket out = new DatagramPacket(bufOut, bufOut.length, recv,recvPort);
+        DatagramPacket out = new DatagramPacket(bufOut, bufOut.length, recv, recvPort);
         serverSocket.send(out);
     }
 
